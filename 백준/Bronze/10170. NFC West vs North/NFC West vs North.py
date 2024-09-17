acf3a0e493a991ea07c9a79324ceca7d663d402d
예빈 <162901124+dpqlsn@users.noreply.github.com>
2024-09-17 23:59:29 +0900

    [Bronze V] Title: NFC West vs North, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.py
@@ -0,0 +1,16 @@
+def print_nfc_standings():
+    print("NFC West       W   L  T")
+    print("-----------------------")
+    print("Seattle        13  3  0")
+    print("San Francisco  12  4  0")
+    print("Arizona        10  6  0")
+    print("St. Louis      7   9  0")
+    
+    print("\nNFC North      W   L  T")
+    print("-----------------------")
+    print("Green Bay      8   7  1")
+    print("Chicago        8   8  0")
+    print("Detroit        7   9  0")
+    print("Minnesota      5  10  1")
+
+print_nfc_standings()
