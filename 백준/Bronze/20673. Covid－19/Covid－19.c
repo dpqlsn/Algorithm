ca4701f113b30aca91bc8dd200e8f59af5516cbb
예빈 <162901124+dpqlsn@users.noreply.github.com>
2024-11-14 13:30:05 +0900

    [Bronze IV] Title: Covid-19, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20673. Covid－19/Covid－19.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int p, q;
+    scanf("%d %d", &p, &q);
+
+    printf("%s", (p <= 50 && q <= 10) ? "White" : (q > 30 ? "Red" : "Yellow"));
+
+    return 0;
+}
