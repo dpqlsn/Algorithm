5a351208b98e534d5ce99fba4e9bbc4992d46938
예빈 <162901124+dpqlsn@users.noreply.github.com>
2025-02-12 22:05:13 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int T, S;
+    scanf("%d %d", &T, &S);
+    
+    if (T >= 12 && T <= 16 && S == 0) {
+        printf("320\n");
+    } else printf("280\n");
+    return 0;
+}
