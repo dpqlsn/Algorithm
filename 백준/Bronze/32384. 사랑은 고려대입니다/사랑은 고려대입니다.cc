3f8e663afc5567e3629bc0aa417672bcc6028b91
예빈 <162901124+dpqlsn@users.noreply.github.com>
2025-04-23 16:53:40 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void sol(int n) {
+    for (int i = 0; i < n; i++) {
+        printf("LoveisKoreaUniversity ");
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    sol(n);
+    return 0;
+}
