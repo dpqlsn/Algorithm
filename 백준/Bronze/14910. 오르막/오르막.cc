6ac299136ea9bcd862e29777c4199efa54b323a8
김예빈 <162901124+dpqlsn@users.noreply.github.com>
2025-06-29 12:47:14 +0900

    [Bronze II] Title: 오르막, Time: 88 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14910. 오르막/오르막.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a;
+    cin >> a;
+
+    while (true) {
+        int b;
+        cin >> b;
+        if (cin.eof()) break;
+        if (a > b) {
+            cout << "Bad" << '\n';
+            return 0;
+        }
+
+        a = b;
+    }
+
+    cout << "Good" << '\n';
+    return 0;
+}
