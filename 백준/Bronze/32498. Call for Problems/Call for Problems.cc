62b1f06c1334402bbf047cdda3247096508140e9
김예빈 <162901124+dpqlsn@users.noreply.github.com>
2025-09-16 22:07:21 +0900

    [Bronze V] Title: Call for Problems, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32498. Call for Problems/Call for Problems.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, a, count = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a);
+        if (a % 2 == 1) {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
