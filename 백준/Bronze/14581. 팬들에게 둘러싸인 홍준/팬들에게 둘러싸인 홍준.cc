c7b1f7ec66ff15e3e363a7d31bb673ca01045bd4
김예빈 <162901124+dpqlsn@users.noreply.github.com>
2025-10-16 00:59:36 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char id[100];
+    scanf("%s", id);
+
+    printf(":fan::fan::fan:\n");
+    printf(":fan::%s::fan:\n", id);
+    printf(":fan::fan::fan:\n");
+
+    return 0;
+}
