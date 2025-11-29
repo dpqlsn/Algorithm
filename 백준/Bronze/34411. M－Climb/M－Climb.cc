2003eba93a4dc948ca1cb4b08d3e829b70cf7751
김예빈 <162901124+dpqlsn@users.noreply.github.com>
2025-11-29 21:09:30 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int m, n, y;
+    scanf("%d", &m);
+    scanf("%d", &n);
+    scanf("%d", &y);
+
+    int cost = m * n;
+    printf("%d", cost);
+
+    return 0;
+}
