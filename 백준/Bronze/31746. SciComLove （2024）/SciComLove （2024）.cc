6bb0bf0efeb184fcd25212715fef7b793587b257
김예빈 <162901124+dpqlsn@users.noreply.github.com>
2026-04-17 11:09:02 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    long long N;
+    scanf("%lld", &N);
+
+    if (N % 2 == 0)
+        printf("SciComLove");
+    else
+        printf("evoLmoCicS");
+
+    return 0;
+}
